7b23c7d4bb338cfd8cbee23c96f2299df2b21dad
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2026-01-14 12:49:33 +0530

    Enhance Navbar with search input and styling
    Updated the Navbar component to include a search input with an icon and improved styling.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,25 +23,37 @@ export default function Navbar() {
               MyLogo
             </Link>
 
-            {/* Right */}
-            <div className="flex items-center   gap-4">
-              <input
-                type="text"
-                placeholder="Search employee..."
-                className="border px-3 py-1 rounded-full text-sm outline-none"
-                onChange={(e) => dispatch(setSearchQuery(e.target.value))}
-              />
-
+            <div className="flex items-center gap-4">
+              {/* SEARCH */}
+              <div className="relative group">
+                <FiSearch
+                  size={18}
+                  className="absolute left-4 top-1/2 -translate-y-1/2
+                 text-gray-400 group-focus-within:text-indigo-600"
+                />
+                <input
+                  type="text"
+                  placeholder="Search employees..."
+                  onChange={(e) => dispatch(setSearchQuery(e.target.value))}
+                  className="w-56 sm:w-64 pl-11 pr-4 py-2 rounded-full
+                 border text-sm transition-all duration-300
+                 focus:w-72 focus:ring-2 focus:ring-indigo-100
+                 focus:border-indigo-500 outline-none"
+                />
+              </div>
 
               {/* ADD BUTTON */}
               <button
                 onClick={() => dispatch(openEmployeePopup())}
-                className="flex cursor-pointer items-center gap-1 bg-indigo-600 text-white px-4 py-2 rounded-full hover:bg-indigo-700"
+                className="flex items-center gap-1 bg-indigo-600
+               text-white px-4 py-2 rounded-full
+               hover:bg-indigo-700 transition"
               >
                 <FiPlus size={18} />
                 <span className="hidden sm:block">Add</span>
               </button>
             </div>
+
 
           </div>
         </div>
